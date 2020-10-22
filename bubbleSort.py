51b07932fd55ea8cf020430598d95ba0d2f24fb0
zisantiano <73284260+zisantiano@users.noreply.github.com>
2020-10-22 18:29:27 +0800

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,12 +1,12 @@
 def bubble_Sort(array): 
-    n = len(array) 
-    for i in range(n-1):   
-        for j in range(0, n-i-1): 
+    m = len(array) 
+    for i in range(m-1):   
+        for j in range(0, m-i-1): 
             if array[j] > array[j+1] : 
                 array[j], array[j+1] = array[j+1], array[j] 
                 
                 
-array = [30, 38, 14, 1, 42, 99, 9,54,87] 
+array = [30, 38, 14, 1, 40, 99, 9,54,87] 
 bubble_Sort(array) 
   
 print ("Sorted arrayay using bubble sort:") 
